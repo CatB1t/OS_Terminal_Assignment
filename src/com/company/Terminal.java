03d4702698cb320fcb1401fe89262b91f0b6c72d
Esraa Yazid <60916510+esraa9322@users.noreply.github.com>
2021-11-17 19:43:27 +0200

    Terminal

--- src/com/company/Terminal.java
+++ src/com/company/Terminal.java
@@ -1,10 +1,47 @@
 package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+class Parser {
+
+    String commandName;
+    String[] args;
+
+    //This method will divide the input into commandName and args
+    //where "input" is the string command entered by the user
+    public boolean parse(String input)
+    {
+        // set the local args equal to what the user input
+        String[] currentWords = input.split(" ");
+
+        commandName = currentWords[0];
+        args = Arrays.copyOfRange(currentWords, 1, currentWords.length);
+        return true;
+    }
+
+    public String getCommandName()
+    {
+        //
+        return commandName;
+    }
+
+    public String[] getArgs()
+    {
+        return args;
+    }
+}
+
 
 public class Terminal {
 
@@ -27,47 +64,38 @@ public class Terminal {
         return true;
     }
 
-    public void echo(String[] args)
-    {
-        if(args.length == 0) {
+    public void echo(String[] args) {
+        if (args.length == 0) {
             showErrorMessage();
             return;
         }
 
         String stringToPrint = "";
-        for(String arg : args)
+        for (String arg : args)
             stringToPrint += arg + " ";
         System.out.println(stringToPrint);
     }
 
-    public void pwd()
-    {
+    public void pwd() {
         System.out.println(currentPath.toAbsolutePath().toString());
     }
 
-    public void cd(String[] args)
-    {
-        if(args.length == 0) // Case no args
+    public void cd(String[] args) {
+        if (args.length == 0) // Case no args
             currentPath = Paths.get(System.getProperty("user.home"));
-        else if(args.length == 1)
-        {
+        else if (args.length == 1) {
             String arg = args[0];
-            if(arg.equals("..")) {
+            if (arg.equals("..")) {
                 currentPath = currentPath.getParent().toAbsolutePath();
-            }
-            else
-            {
-                if(isValidPath(arg))
-                {
-                    currentPath = Paths.get(arg);
-                }
-                else {
+            } else {
+                if (isValidPath(arg)) {
+                    currentPath = currentPath.resolve(arg);
+                } else {
                     System.out.println("Not a valid path.");
                     return;
                 }
             }
-        }
-        else {
+        } else {
             showErrorMessage();
             return;
         }
@@ -75,13 +103,190 @@ public class Terminal {
         System.out.println("Current Path: " + currentPath.toString());
     }
 
+    public void ls() {
+        String[] files;
+        File file = currentPath.toFile();
+        files = file.list();
+        Arrays.sort(files);
+        for (int i = 0; i < files.length; i++) {
+            System.out.print(files[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void ls_r() {
+        String[] files;
+        File file = currentPath.toFile();
+        files = file.list();
+        Arrays.sort(files);
+        for (int i = files.length - 1; i >= 0; i--) {
+            System.out.print(files[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void mkdir(String[] string) {
+        for (String i : string) {
+
+            if (!currentPath.resolve(i).toFile().mkdir()) {
+                System.out.println("Error creating directory");
+            }
+        }
+    }
+
+    public void rm(String[] string) {
+        File file = currentPath.resolve(string[0]).toFile();
+        if (file.exists()) {
+            file.delete();
+        } else {
+            System.out.println("No such file!");
+        }
+    }
+
+    public void cat(String[] string) {
+        if (string.length == 1) {
+            File file = currentPath.resolve(string[0]).toFile();
+            if (file.exists()) {
+                if (file.isDirectory()) {
+                    System.out.println("No such file!");
+                } else {
+                    Scanner input = null;
+                    try {
+                        input = new Scanner((file));
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Error");
+                        return;
+
+                    }
+                    while (input.hasNextLine()) {
+                        System.out.println(input.nextLine());
+                    }
+                }
+            } else {
+                System.out.println("No such file!");
+            }
+        } else if (string.length == 2) {
+            File firstFile = currentPath.resolve(string[0]).toFile();
+            File secondFile = currentPath.resolve(string[1]).toFile();
+            if (!(firstFile.exists() && secondFile.exists())) {
+                System.out.println("No such file(s)!");
+            } else {
+                Scanner input = null;
+                try {
+                    input = new Scanner((firstFile));
+                } catch (FileNotFoundException e) {
+                    System.out.println("Error");
+                    return;
+
+                }
+                while (input.hasNextLine()) {
+                    System.out.println(input.nextLine());
+                }
+
+                try {
+                    input = new Scanner((secondFile));
+                } catch (FileNotFoundException e) {
+                    System.out.println("Error");
+                    return;
+
+                }
+                while (input.hasNextLine()) {
+                    System.out.println(input.nextLine());
+                }
+            }
+        } else {
+            System.out.println(": No such file(s)!");
+        }
+    }
+
+
+    public void rmdir(String[] string) {
+        File[] files;
+        if (string[0].equals("*")) {
+            File file = currentPath.toFile();
+            files = file.listFiles();
+            for (File path : files) {
+                if (path.list() != null && path.list().length == 0) {
+                    path.delete();
+                }
+            }
+        } else {
+            File file = currentPath.resolve(string[0]).toFile();
+            if (file.exists()) {
+                files = file.listFiles();
+                if (file.list() != null && file.list().length == 0) {
+                    file.delete();
+                } else {
+                    System.out.println("Error! Selected directory is not empty");
+                }
+            } else {
+                System.out.println("No such directory!");
+            }
+        }
+    }
+
+    public void touch(String[] string) {
+        try {
+            currentPath.resolve(string[0]).toFile().createNewFile();
+        } catch (IOException e) {
+            System.out.println("Error while creating");
+        }
+    }
+
+    public void cp(String[] string) {
+        File firstFile = currentPath.resolve(string[0]).toFile();
+        File secondFile = currentPath.resolve(string[1]).toFile();
+        if (firstFile.isDirectory() && secondFile.isDirectory()) {
+            System.out.println("No such files!");
+        } else {
+            if (firstFile.exists() ) {
+                try {
+                    Scanner reader = new Scanner(firstFile);
+                    FileWriter writer = null;
+                    writer = new FileWriter(secondFile);
+                    while (reader.hasNextLine()) {
+                        writer.write(reader.nextLine());
+                        writer.write("\n");
+                    }
+                    reader.close();
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println("Error while copying");
+
+                }
+            } else {
+                System.out.println("No such files!");
+            }
+        }
+    }
+
+    public void cp_r(String[] string) {
+        Path firstDir = currentPath.resolve(string[1]);
+        Path secondDir = currentPath.resolve(string[2]).resolve(firstDir.getFileName());
+
+
+        try (Stream<Path> stream = Files.walk(firstDir)) {
+            stream.forEachOrdered(sourcePath -> {
+
+                try {
+                    Files.copy(sourcePath, secondDir.resolve(firstDir.relativize(sourcePath)));
+                } catch (IOException e) {
+                    System.out.println("Error while copying");
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+          //  System.out.println("Error while copying");
+            e.printStackTrace();
+        }
+    }
+
+
     //This method will choose the suitable command method to be called
-    public void chooseCommandAction()
-    {
+    public void chooseCommandAction() {
         String userInput = inputScanner.nextLine();
         parser.parse(userInput);
-        switch(parser.commandName)
-        {
+        switch (parser.commandName) {
             case "echo":
                 echo(parser.args);
                 break;
@@ -94,14 +299,44 @@ public class Terminal {
             case "exit":
                 shouldExit = true;
                 break;
+            case "ls":
+                if (parser.args.length == 1 && parser.args[0].equals("-r")) {
+                    ls_r();
+                } else {
+                    ls();
+                }
+                break;
+            case "mkdir":
+                mkdir(parser.args);
+                break;
+            case "rmdir":
+                rmdir(parser.args);
+                break;
+            case "touch":
+                touch(parser.args);
+                break;
+            case "cp":
+                if (parser.args.length == 3 && parser.args[0].equals("-r")) {
+                    cp_r(parser.args);
+                } else {
+                    cp(parser.args);
+                }
+                break;
+            case "rm":
+                rm(parser.args);
+                break;
+            case "cat":
+                cat(parser.args);
+                break;
+
+
             default:
                 showErrorMessage();
                 break;
         }
     }
 
-    public Terminal()
-    {
+    public Terminal() {
         inputScanner = new Scanner(System.in);
         parser = new Parser();
         currentPath = Paths.get("").toAbsolutePath();
@@ -110,16 +345,13 @@ public class Terminal {
         System.out.println("Starting Terminal ... Ready.");
     }
 
-    public void Run()
-    {
-        while(!shouldExit)
-        {
+    public void Run() {
+        while (!shouldExit) {
             chooseCommandAction();
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Terminal terminal = new Terminal();
         terminal.Run();
     }
